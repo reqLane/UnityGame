95ed3a616ae7536a39226b1f57b4582724b19a8a
AMykolaD <andrusenkomykola@gmail.com>
2022-11-11 12:26:55 +0300

    Fixed crab intelligence

--- Assets/Scripts/Enemies/CrabEnemy.cs
+++ Assets/Scripts/Enemies/CrabEnemy.cs
@@ -10,7 +10,7 @@ public class CrabEnemy : GroundEnemy
     // Start is called before the first frame update
     void Start()
     {
-        enemyStats.facesRight = Random.Range(0, 1) == 1 ? true : false;
+        enemyStats.facesRight = Random.Range(0, 2) == 1 ? true : false;
         if(!enemyStats.facesRight) transform.localScale = new Vector3(-1 * Math.Abs(transform.localScale.x), Math.Abs(transform.localScale.y), 0);
         enemyStats.collider = GetComponent<Collider2D>();
     }
@@ -20,15 +20,15 @@ public class CrabEnemy : GroundEnemy
     {
         helpFeetPoint.x = enemyStats.feetPos.x + (enemyStats.facesRight ? 1 : -1) * enemyStats.collider.bounds.size.x / 4;
         helpFeetPoint.y = enemyStats.feetPos.y;
-        helpFrontPoint1.x = transform.position.x + (enemyStats.facesRight ? 1 : -1) * enemyStats.collider.bounds.size.x / 2;
-        helpFrontPoint1.y = enemyStats.feetPos.y + enemyStats.collider.bounds.size.y / 10;
-        helpFrontPoint2.x = helpFrontPoint1.x;
-        helpFrontPoint2.y = helpFrontPoint1.y + enemyStats.collider.bounds.size.y * 0.8f;
-        enemyStats.feetPos.x = transform.position.x;
-        enemyStats.feetPos.y = transform.position.y - enemyStats.collider.bounds.size.y / 2;
+        helpFrontPoint1.x = enemyStats.collider.transform.position.x + (enemyStats.facesRight ? 1 : -1) * enemyStats.collider.bounds.size.x / 3;
+        helpFrontPoint1.y = enemyStats.feetPos.y + enemyStats.collider.bounds.size.y / 20;
+        helpFrontPoint2.x = helpFrontPoint1.x+ (enemyStats.facesRight ? 1 : -1)*0.2f* enemyStats.collider.bounds.size.x;
+        helpFrontPoint2.y = helpFrontPoint1.y + enemyStats.collider.bounds.size.y * 0.95f;
+        enemyStats.feetPos.x = enemyStats.collider.transform.position.x;
+        enemyStats.feetPos.y = enemyStats.collider.transform.position.y - enemyStats.collider.bounds.size.y / 2;
         if (Physics2D.OverlapCircle(enemyStats.feetPos, 0.01f, LayerMask.GetMask("Collidable")) != null)
         {
-            if(Physics2D.OverlapCircle(helpFeetPoint, 0.01f, LayerMask.GetMask("Collidable")) == null||(Physics2D.OverlapArea(helpFrontPoint1, helpFrontPoint2, LayerMask.GetMask("Collidable")) !=null&& (Physics2D.OverlapArea(helpFrontPoint1, helpFrontPoint2, LayerMask.GetMask("Collidable")))))
+            if(Physics2D.OverlapCircle(helpFeetPoint, 0.04f, LayerMask.GetMask("Collidable")) == null||(Physics2D.OverlapArea(helpFrontPoint1, helpFrontPoint2, LayerMask.GetMask("Collidable")) !=null))
             {
                 if (enemyStats.facesRight)
                 {
